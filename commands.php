23a6daa82d44d1ed67c4285fc4fa3874a49acded
Shodiq Daffa <shodiqdaffa2302@gmail.com>
2019-10-14 12:10:26 +0700

    Add divide command to command list

--- commands.php
+++ commands.php
@@ -4,4 +4,5 @@ return [
     'Jakmall\Recruitment\Calculator\Commands\AddCommand',
     'Jakmall\Recruitment\Calculator\Commands\SubtractCommand',
     'Jakmall\Recruitment\Calculator\Commands\MultiplyCommand',
+    'Jakmall\Recruitment\Calculator\Commands\DivideCommand',
 ];
